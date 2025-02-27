831480a1ca40218b859eb5404842d5f62d5c1db1
Sujash-1210 <sujashdas80@gmail.com>
2025-02-27 22:18:33 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+))
 
 //api endpoint
 app.use('/api/admin', adminRouter)    
